f399c757989c0b7fee291f153244e4f094ebb2b6
Alessandro Giustina <alexjusti67n@gmail.com>
2026-01-30 09:36:42 +0100

    feat: added bin2srec source code and platformio env for compilation

---
+++ scripts/srec_cellboard_build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+pio run -t clean
+pio run -e base
+pio run -e bin2srec
+
+./.pio/build/bin2srec/program -o 0x8003000 -a 4 ./.pio/build/base/firmware.bin > ./binaries/cellboard.srec
