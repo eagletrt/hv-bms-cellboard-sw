d13b992a7ff189285c5600555b5a6ba6f6bd3277
Alessandro Giustina <alexjusti67n@gmail.com>
2026-01-30 11:18:22 +0100

    fix: fixed conversion script

--- scripts/srec_cellboard_build.sh
+++ scripts/srec_cellboard_build.sh
@@ -4,4 +4,4 @@ pio run -t clean
 pio run -e base
 pio run -e bin2srec
 
-./.pio/build/bin2srec/program -o 0x8003000 -a 4 ./.pio/build/base/firmware.bin > ./binaries/cellboard.srec
+./.pio/build/bin2srec/program -o 8003000 -a 4 ./.pio/build/base/firmware.bin > ./binaries/cellboard.srec
