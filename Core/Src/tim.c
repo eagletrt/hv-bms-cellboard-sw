87689fdd20b681a149e1f0536b787b5c0121a9f8
Antonio Gelain <antonio.gelain2@gmail.com>
2026-05-23 01:09:32 +0200

    fix: remove timer utils dependency

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -22,8 +22,6 @@
 
 /* USER CODE BEGIN 0 */
 
-#include "timer_utils.h"
-
 #include "timebase.h"
 #include "error-api.h"
 
@@ -212,6 +210,32 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *tim_baseHandle) {
 }
 
 /* USER CODE BEGIN 1 */
+
+uint32_t TIM_GetInternalClkFreq(TIM_HandleTypeDef *htim) {
+    RCC_ClkInitTypeDef clkConfig;
+    uint32_t pFlatency;
+    uint32_t uwTimClock, uwAPBxPrescaler = 0U;
+
+    /* Get clock configuration */
+    HAL_RCC_GetClockConfig(&clkConfig, &pFlatency);
+
+    /* Get APBx prescaler and base clock*/
+    if (_M_GET_TIM_APB_PLACEMENT(htim)) {
+        uwAPBxPrescaler = clkConfig.APB1CLKDivider;
+        uwTimClock = HAL_RCC_GetPCLK1Freq();
+    } else {
+        uwAPBxPrescaler = clkConfig.APB2CLKDivider;
+        uwTimClock = HAL_RCC_GetPCLK2Freq();
+    }
+
+    /* Compute final clock */
+    if (uwAPBxPrescaler != RCC_HCLK_DIV1) {
+        /* When the prescaler is not one the frequncy is multipled by 2 for timers */
+        uwTimClock *= 2;
+    }
+
+    return uwTimClock;
+}
 
 /**
  * @brief Callback that updates the error timer based on a timestamp and a timeout
